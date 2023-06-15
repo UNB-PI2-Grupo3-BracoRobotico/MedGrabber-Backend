aad4aee3762ca0bb8721b4219ca98615212472a3
savioc2 <saviocunha61@gmail.com>
2023-06-15 18:57:04 -0300

    Corrigindo configuração

---
+++ src/config/database.py
@@ -0,0 +1,9 @@
+from os import environ as env
+
+DATABASE_CONNECTION = {
+    "dbname": env.get("DB_NAME", "grabber"),
+    "user": env.get("DB_USER", "postgres",
+    "password": env.get("DB_PASSWORD", "pass",
+    "host": env.get("DB_HOST", "localhost",
+    "port": env.get("DB_PORT", "5432",
+}
