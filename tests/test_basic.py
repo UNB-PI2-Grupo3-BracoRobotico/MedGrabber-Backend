a8a6d31a1af83a578ae098a7d9e4e979af95b876
fmaachadoo <filipe__machado@hotmail.com>
2023-06-15 10:12:16 -0300

    chore: add dummy test to test if test suit is working

---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+import pytest
+
+def test_add_two_numbers():
+    """
+    Dummy test to check if the test suite is working
+    """
+    result = 2 + 3
+    assert result == 5, "The result of adding 2 and 3 should be 5"
