e6ec295627710a35eca0a6ef46c081b600fb29dc
savioc2 <saviocunha61@gmail.com>
2023-06-17 19:44:17 -0300

    adicionando tabelas e atributos
    PedroMoraes39 <phcmoraes39@gmail.com>

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,15 +1,84 @@
+-- Adição de tabelas
 CREATE TABLE IF NOT EXISTS Produto (
     id INT,
-    nome VARCHAR(50),
+    nome VARCHAR(100),
     preco FLOAT,
     descricao VARCHAR(100),
     PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS Pagamento (
+CREATE TABLE IF NOT EXISTS Inventario (
     id INT,
-    consumidor VARCHAR(50),
-    preco FLOAT,
-    TipoPagamento VARCHAR(100),
+    id_Produto INT,
+    localizacaoX POINT,
+    localizacaoy POINT,
+    quantidade INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_Produto FOREIGN KEY (id_Produto) REFERENCES Produto(id)
+);
+
+CREATE TABLE IF NOT EXISTS Itens_da_compra (
+    id INT,
+    id_Produto INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_produto FOREIGN KEY(id_Produto) REFERENCES Produto(id)
+);
+
+CREATE TABLE IF NOT EXISTS Carrinho (
+    id INT,
+    id_Inventario INT,
+    id_Itens INT,
+    total FLOAT,
+    grupo integer not null default 0,
+    PRIMARY KEY(id), 
+    CONSTRAINT fk_inventario FOREIGN KEY (id_Inventario) REFERENCES Inventario(id),
+    CONSTRAINT fk_Itens FOREIGN KEY (id_Itens) REFERENCES Itens_da_compra(id)
+);
+
+CREATE TABLE IF NOT EXISTS Pagamento(
+    id INT,
+    id_Itens INT,
+    status_pagamento integer not null default 0,
+    consumidor VARCHAR(100),
+    tipoPagamento INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_Itens FOREIGN KEY (id_Itens) REFERENCES Itens_da_compra(id)
+);
+
+CREATE TABLE IF NOT EXISTS Usuario(
+    cpf INT,
+    nome VARCHAR(100),
+    login_usuario VARCHAR(100),
+    senha VARCHAR(100),
+    PRIMARY KEY(cpf)
+);
+
+CREATE TABLE IF NOT EXISTS Estoquista(
+    id INT,
+    controleEstoque VARCHAR(100),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE IF NOT EXISTS Pedidos(
+    id INT,
+    id_Pagamento INT,
+    id_Carrinho INT,
+    id_Usuario INT,
+    data_pedido INT,
+    tipo VARCHAR(50),
+    PRIMARY KEY(id),
+    CONSTRAINT fk_Pagamento FOREIGN KEY (id_Pagamento) REFERENCES Pagamento(id),
+    CONSTRAINT fk_Carrinho FOREIGN KEY (id_Carrinho) REFERENCES Carrinho(id),
+    CONSTRAINT fk_Usuario FOREIGN KEY (id_Usuario) REFERENCES Usuario(cpf)
+);
+
+CREATE TABLE IF NOT EXISTS consumidor(
+    id INT,
+    id_Carrinho INT,
+    id_Pagamento INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_Carrinho FOREIGN KEY (id_Carrinho) REFERENCES Carrinho(id),
+    CONSTRAINT fk_Pagamento FOREIGN KEY (id_Pagamento) REFERENCES Pagamento(id)
+);
+
+
