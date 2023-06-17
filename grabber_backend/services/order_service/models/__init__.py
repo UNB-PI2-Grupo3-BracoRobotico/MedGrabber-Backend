90d5e176662ead2947ab346b3f692ab53678e405
fmaachadoo <filipe__machado@hotmail.com>
2023-06-17 17:34:42 -0300

    refactor: apply lint

--- grabber_backend/services/order_service/models/__init__.py
+++ grabber_backend/services/order_service/models/__init__.py
@@ -2,14 +2,16 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
     item_id: int
     quantity: int
 
+
 class Order(BaseModel):
     order_id: int
     customer_id: int
-    status: Optional[str] = 'received'
+    status: Optional[str] = "received"
     item_list: list[Item]
     total_price: str
     payment_method: str
--- grabber_backend/services/order_service/shopping_manager/order_creator.py
+++ grabber_backend/services/order_service/shopping_manager/order_creator.py
@@ -1,33 +1,29 @@
 import logging
 
+
 class OrderCreator:
-    
     def __init__(self, order):
         self.logger = logging.getLogger(__name__)
         self.db_connection = self.get_db_connection()
-        self.order = order
-    
+        self.order = self.validate_order(order)
+
     def get_db_connection(self):
-        # db_connection = psycopg2.connect(
-        #     dbname="grabber",
-        #     user="postgres",
-        #     password="pass",
-        #     host="localhost",
-        #     port="5432",
-        # )
-        # return db_connection
-        return True # This is just a fake
-    
+        # TODO: use the database_controller package
+        return True  # This is just a fake
+
     def validate_order(self):
-        self.logger.info('Validating order')
+        self.logger.info("Validating order")
+
         pass
-    
+
     def create_order(self):
-        self.logger.info('Creating order in database')
+        self.logger.info("Creating order in database")
         pass
-    
+
     def receive_order(self):
-        if self.validate_order():
-            self.create_order()
-            return True
-        
+        if self.order:
+            order_data = self.create_order()
+            return order_data
+        else:
+            self.logger.info("Order is not valid")
+            return None
