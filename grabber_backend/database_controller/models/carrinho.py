4a96aceada88736457d657549f69bdacff8558f8
Samuel Castro <samuelcastro2010@gmail.com>
2023-06-26 19:55:55 -0300

    Tentativa para teste da model
    Tentativa de uma maneira diferente de implementar uma classe para ver se vai passar ou não nos testes.

--- grabber_backend/database_controller/models/carrinho.py
+++ grabber_backend/database_controller/models/carrinho.py
@@ -1,9 +1,39 @@
-class Carrinho:
-    def __init__(self, id_Produto, produto, total, id=None):
-        self.id = id
-        self.id_Produto = id_Produto
-        self.produto = produto
-        self.total = total
+from sqlalchemy import Column, Integer, Float
+from sqlalchemy.ext.declarative import declarative_base
+import json
+
+Base = declarative_base()
+
+class Cart(Base):
+    __tablename__ = "Cart"
+
+    id = Column("id", Integer, primary_key=True)
+    product = Column("product_id", Integer, nullable=False)
+    total = Column("total", Float)
+
+    def __eq__(self, other):
+        if not isinstance(other, Cart):
+            return False
+        
+        return (
+            self.id == other.id and
+            self.product == other.product and
+            self.total == other.total
+        )
     
     def __str__(self):
-        return f"Carrinho[id={self.id}, id_Produto={self.id_Produto}, produto={self.produto}, total={self.total}]"
+        return json.dumps({
+            "id": self.id,
+            "product": self.product,
+            "total": self.total
+        })
+
+# class Cart:
+#     def __init__(self, product_id, product, total, id=None):
+#         self.id = id
+#         self.product_id = product_id
+#         self.product = product
+#         self.total = total
+    
+#     def __str__(self):
+#         return f"Cart[id={self.id}, product_id={self.product_id}, product={self.product}, total={self.total}]"
