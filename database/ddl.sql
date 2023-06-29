5d2379ebf0b94e4fc753e108ba101bf162298942
fmaachadoo <filipe__machado@hotmail.com>
2023-06-29 06:43:53 -0300

    fix: change order_product primary key

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,15 +1,68 @@
-CREATE TABLE IF NOT EXISTS Produto (
-    id INT,
-    nome VARCHAR(50),
-    preco FLOAT,
-    descricao VARCHAR(100),
-    PRIMARY KEY(id)
+CREATE TYPE user_role_type as ENUM ('customer', 'stock_manager')
+
+CREATE TYPE order_status_type as ENUM (
+    'created', 'pending', 'paid', 'separation', 'delivered', 'canceled'
+)
+
+CREATE TYPE payment_status_type as ENUM ('pending', 'paid', 'canceled')
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(50) NOT NULL,
+    email VARCHAR(50),
+    store_name VARCHAR(50),
+    personal_name VARCHAR(50) NOT NULL,
+    machine_serial_number VARCHAR(50),
+    phone_number VARCHAR(50) UNIQUE,
+    user_role user_role_type
 );
 
-CREATE TABLE IF NOT EXISTS Pagamento (
-    id INT,
-    consumidor VARCHAR(50),
-    preco FLOAT,
-    TipoPagamento VARCHAR(100),
-    PRIMARY KEY(id)
+CREATE TABLE product (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(50),
+    product_description VARCHAR(300),
+    product_price DECIMAL(10,2),
+    modified_by INTEGER,
+    modified_at TIMESTAMP,
+    FOREIGN KEY (modified_by) REFERENCES users(user_id)
+);
+
+CREATE TABLE order (
+    order_id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    order_date DATE,
+    total_cost DECIMAL(10,2),
+    order_status order_status_type,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE order_product (
+    order_id INTEGER,
+    product_id INTEGER,
+    product_amount INTEGER,
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES order(order_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
+CREATE TABLE payment (
+    payment_id SERIAL PRIMARY KEY,
+    order_id INTEGER,
+    payment_date DATE,
+    payment_method VARCHAR(50),
+    payment_status payment_status_type,
+    FOREIGN KEY (order_id) REFERENCES order(order_id)
+);
+
+CREATE TABLE position (
+    position_id SERIAL PRIMARY KEY,
+    position_x INTEGER,
+    position_y INTEGER,
+    product_id INTEGER,
+    product_amount INTEGER,
+    modified_by INTEGER,
+    modified_at TIMESTAMP,
+    FOREIGN KEY (modified_by) REFERENCES users(user_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
