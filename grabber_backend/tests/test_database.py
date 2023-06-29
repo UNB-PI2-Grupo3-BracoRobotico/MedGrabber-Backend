5b1ad2720a037c783cc762ada39d42dc91d5bb91
fmaachadoo <filipe__machado@hotmail.com>
2023-06-29 09:10:09 -0300

    refactor: apply black link

--- grabber_backend/tests/test_database.py
+++ grabber_backend/tests/test_database.py
@@ -4,17 +4,25 @@ import logging
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from grabber_backend.database_controller.models import User, Product, Order, OrderProduct, Payment, Position, UserRoleEnum
+from grabber_backend.database_controller.models import (
+    User,
+    Product,
+    Order,
+    OrderProduct,
+    Payment,
+    Position,
+    UserRoleEnum,
+)
 from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 import psycopg2
 from psycopg2 import sql
 
 
-TEST_ENGINE_URL = 'postgresql://postgres:pass@db/grabber'
+TEST_ENGINE_URL = "postgresql://postgres:pass@db/grabber"
 
 # GET dinamic ddl path
-DDL_PATH = os.path.join(os.path.dirname(__file__), '../../', 'database', 'ddl.sql')
+DDL_PATH = os.path.join(os.path.dirname(__file__), "../../", "database", "ddl.sql")
 
 # create a new engine and session
 # engine = create_engine(TEST_ENGINE_URL)
@@ -33,16 +41,16 @@ def test_user():
     session = Session()
 
     new_user = User(
-        username='test_user',
-        password_hash='test_hash',
-        personal_name='Test User',
+        username="test_user",
+        password_hash="test_hash",
+        personal_name="Test User",
         user_role=UserRoleEnum.customer,
-        phone_number='123456789'
+        phone_number="123456789",
     )
     session.add(new_user)
     session.commit()
 
-    user = session.query(User).filter_by(username='test_user').first()
+    user = session.query(User).filter_by(username="test_user").first()
     assert user is not None
 
     session.close()
--- grabber_backend/tests/utils.py
+++ grabber_backend/tests/utils.py
@@ -4,17 +4,25 @@ import logging
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from grabber_backend.database_controller.models import User, Product, Order, OrderProduct, Payment, Position, UserRoleEnum
+from grabber_backend.database_controller.models import (
+    User,
+    Product,
+    Order,
+    OrderProduct,
+    Payment,
+    Position,
+    UserRoleEnum,
+)
 from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 import psycopg2
 from psycopg2 import sql
 
 
-TEST_ENGINE_URL = 'postgresql://postgres:pass@db/grabber'
+TEST_ENGINE_URL = "postgresql://postgres:pass@db/grabber"
 
 # GET dinamic ddl path
-DDL_PATH = os.path.join(os.path.dirname(__file__), '../../', 'database', 'ddl.sql')
+DDL_PATH = os.path.join(os.path.dirname(__file__), "../../", "database", "ddl.sql")
 
 # create a new engine and session
 # engine = create_engine(TEST_ENGINE_URL)
@@ -29,10 +37,7 @@ def setup():
     global engine, Session
     # Use the psycopg2 library to connect to the PostgreSQL server
     conn = psycopg2.connect(
-        dbname='postgres',
-        user='postgres',
-        host='db',
-        password='pass'
+        dbname="postgres", user="postgres", host="db", password="pass"
     )
 
     # Auto-commit must be enabled to create databases
@@ -42,7 +47,7 @@ def setup():
     cur = conn.cursor()
 
     # Use SQL string composition to create a new database
-    cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier('test_db')))
+    cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier("test_db")))
 
     # Close communication with the database
     cur.close()
@@ -50,16 +55,13 @@ def setup():
 
     # Connect to the new test_db database to run migrations
     conn = psycopg2.connect(
-        dbname='test_db',
-        user='postgres',
-        host='db',
-        password='pass'
+        dbname="test_db", user="postgres", host="db", password="pass"
     )
     cur = conn.cursor()
 
     # Assume you have an DDL SQL file to create all necessary tables
     # and other database structures
-    with open(DDL_PATH, 'r') as f:
+    with open(DDL_PATH, "r") as f:
         cur.execute(f.read())
 
     # Close communication with the database
@@ -71,13 +73,16 @@ def teardown():
     global engine
     # Dispose the engine used for test_db and connect to the Postgres server
     engine.dispose()
-    engine = create_engine('postgresql://postgres:pass@db:5432/postgres')
+    engine = create_engine("postgresql://postgres:pass@db:5432/postgres")
 
     with engine.connect() as connection:
         connection.execution_options(isolation_level="AUTOCOMMIT")
         # Terminate all connections to the test_db
-        connection.execute(text('SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = \'test_db\' AND pid <> pg_backend_pid()'))
+        connection.execute(
+            text(
+                "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'test_db' AND pid <> pg_backend_pid()"
+            )
+        )
 
         # Drop the test_db
-        connection.execute(text('DROP DATABASE test_db'))
-
+        connection.execute(text("DROP DATABASE test_db"))
